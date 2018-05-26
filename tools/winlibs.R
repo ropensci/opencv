24ce75cfe04f02965c312ea18dbda8c57a4d62b5
Jeroen Ooms <jeroenooms@gmail.com>
2018-05-26 15:35:04 +0200

    Adding rwinlib script

---
+++ tools/winlibs.R
@@ -0,0 +1,11 @@
+# We need https
+if(getRversion() < "3.3.0") setInternet2()
+VERSION <- commandArgs(TRUE)
+
+# Downloads libssh2 + dependencies
+if(!file.exists(sprintf("../windows/opencv-%s/include/opencv2/opencv.hpp", VERSION))){
+  download.file(sprintf("https://github.com/rwinlib/opencv/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+  dir.create("../windows", showWarnings = FALSE)
+  unzip("lib.zip", exdir = "../windows")
+  unlink("lib.zip")
+}
