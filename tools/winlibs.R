9d13d5d3a6b9732b6bb437d662b9946312724e7d
Jeroen Ooms <jeroenooms@gmail.com>
2019-03-30 17:17:49 +0100

    Fix include path

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -3,7 +3,7 @@ if(getRversion() < "3.3.0") setInternet2()
 VERSION <- commandArgs(TRUE)
 
 # Downloads libssh2 + dependencies
-if(!file.exists(sprintf("../windows/opencv-%s/include/opencv2/opencv.hpp", VERSION))){
+if(!file.exists(sprintf("../windows/opencv-%s/include/opencv4/opencv2/opencv.hpp", VERSION))){
   download.file(sprintf("https://github.com/rwinlib/opencv/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
