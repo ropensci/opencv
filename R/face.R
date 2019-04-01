40c619ae8f613f26af9c77a675d3b1f498a992da
Jeroen Ooms <jeroenooms@gmail.com>
2019-04-01 14:30:13 +0200

    Merge manual pages for CRAN

--- R/face.R
+++ R/face.R
@@ -1,16 +1,5 @@
-#' Face Detection
-#'
-#' Find faces and eyes in a picture.
-#'
 #' @export
-#' @rdname ocv_face
-#' @inheritParams ocv_read
-#' @examples \donttest{
-#' unconf <- ocv_read('https://jeroen.github.io/images/unconf18.jpg')
-#' faces <- ocv_face(unconf)
-#' facemask <- ocv_facemask(unconf)
-#' attr(facemask, 'faces')
-#' }
+#' @rdname opencv
 ocv_face <- function(image){
   facedata <- find_data_xml('haarcascades/haarcascade_frontalface_alt.xml')
   eyedata <- find_data_xml('haarcascades/haarcascade_eye_tree_eyeglasses.xml')
