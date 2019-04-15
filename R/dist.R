8daac9845bb3940b92d3bfa4f68f8b729c8ffdf6
Geoffrey Thompson <gzthompson@gmail.com>
2019-04-15 18:06:52 -0500

    add documentation to binary image function

--- R/dist.R
+++ R/dist.R
@@ -2,15 +2,21 @@
 
 #' Distance Transform for Binary Files
 #'
-#' @param matrix
+#' @param image A binary image
+#' @param distancetype Type of distance, see cv::DistanceTypes
+#' @param masksize Size of the distance transform mask, see cv::DistanceTransformMasks.
+#'      In case of the DIST_L1 or DIST_C distance type, the
+#'      parameter is forced to 3 because a 3×3 mask gives the same result as 5×5 or any
+#'      larger aperture.
 #'
-#' @return
+#' @return Output image with calculated distances. It is a 8-bit or 32-bit
+#'        floating-point, single-channel image of the same size as src .
 #' @export
 #'
 #' @examples
-dist_transform <- function(image){
+dist_transform <- function(image, distancetype = 3, masksize = 3){
 
-  cv_dist_transform((image), 1,3, 3)
+  cv_dist_transform((image), distancetype,masksize, 5)
 }
 
 #' Grayscale version of image
