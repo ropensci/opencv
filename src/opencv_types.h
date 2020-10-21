03dcacc6712209de5492ed761f40bb9269b4b2dc
Jeroen Ooms <jeroenooms@gmail.com>
2020-10-21 19:05:26 +0200

    Need to include headers before macro

--- src/opencv_types.h
+++ src/opencv_types.h
@@ -1,6 +1,9 @@
 #define R_NO_REMAP
 #define STRICT_R_HEADERS
 
+#include <Rcpp.h>
+#include <opencv2/opencv.hpp>
+
 #if CV_VERSION_EPOCH >= 3 || CV_VERSION_MAJOR >= 3
 #define HAVE_OPENCV_3
 #endif
@@ -8,9 +11,6 @@
 #if CV_VERSION_EPOCH >= 4 || CV_VERSION_MAJOR >= 4
 #define HAVE_OPENCV_4
 #endif
-
-#include <Rcpp.h>
-#include <opencv2/opencv.hpp>
 
 void finalize_mat(cv::Mat * frame);
 typedef Rcpp::XPtr<cv::Mat, Rcpp::PreserveStorage, finalize_mat, true> XPtrMat;
