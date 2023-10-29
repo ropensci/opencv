c4acf1c8e0eb8b3391202eb8ffdcd26336ca4aee
Jeroen Ooms <jeroenooms@gmail.com>
2023-10-29 12:44:20 +0100

    Update docs

--- R/opencv.R
+++ R/opencv.R
@@ -39,7 +39,7 @@ ocv_read <- function(path){
 
 #' @export
 #' @rdname opencv
-#' @param image an ocv image object
+#' @param image an ocv image object created from e.g. [ocv_read()]
 ocv_write <- function(image, path){
   path <- normalizePath(path, mustWork = FALSE)
   cvmat_write(image, path)
--- R/qr.R
+++ R/qr.R
@@ -1,29 +1,42 @@
-#' Detect QR codes
+#' Detect and Decode a QR code
 #'
-#' Detect a QR code from an image. The `ocv_qr_detect` function returns the text
-#' content from the QR, and `ocv_qr_mask` returns an annotated image with the QR
-#' area highlighted, and text value as an attribute.
+#' Detect and decode a QR code from an image or camera. By default it returns
+#' the text value from the QR code if detected, or NULL if no QR was found. If
+#' `draw = TRUE` then it returns an annotated image with the position and
+#' value of the QR drawn into the image, and qr text value as an attribute.
+#'
+#' OpenCV has two separate QR decoders. The 'wechat' decoder was added in
+#' libopencv 4.5.2 and generally has better performance and fault-tolerance.
+#' The old 'quirc' decoder is available on some older versions of libopencv as
+#' a plug-in, but many Linux distros did not include it. If you get an error
+#' *Library QUIRC is not linked. No decoding is performed.* this sadly means
+#' your Linux distribution is too old and does not support QR decoding.
 #'
 #' @export
-#' @inheritParams ocv_write
 #' @rdname qrcode
-#' @param decoder which decoder implementation to use. The 'wechat' decoder is
-#' the default and generally has better performance and fault-tolerance.
+#' @inheritParams ocv_write
+#' @param draw if TRUE, the function returns an annotated image showing
+#' the position and value of the QR code.
+#' @param decoder which decoder implementation to use, see details.
+#' @returns if a QR code is detected, this returns either the text value of the QR,
+#' or if `draw` it returns the annotated image, with the value as an attribute.
+#' Returns NULL if no QR was found in the image.
 #' @examples
 #' png("test.png")
 #' plot(qrcode::qr_code("This is a test"))
 #' dev.off()
 #' ocv_qrtext(ocv_read('test.png'))
 #' unlink("test.png")
-ocv_qr_detect <- function(image, return_image = FALSE, decoder = c("wechat", "quirc")){
-  return_image <- as.logical(return_image)
+ocv_qr_detect <- function(image, draw = FALSE, decoder = c("wechat", "quirc")){
+  draw <- as.logical(draw)
   use_wechat <- match.arg(decoder) == 'wechat'
-  cvmat_qr_detect(image, return_image, use_wechat)
+  cvmat_qr_detect(image, draw, use_wechat)
 }
 
 #' @export
 #' @rdname qrcode
-#' @param ... parameters passed to [ocv_qr_detect]
-qr_scanner <- function(...){
-  ocv_video(function(image) ocv_qr_detect(image, ...) , stop_on_result = TRUE)
+qr_scanner <- function(draw = FALSE, decoder = c("wechat", "quirc")){
+  ocv_video(function(image) {
+    ocv_qr_detect(image = image, draw = draw, decoder = decoder)
+  }, stop_on_result = TRUE)
 }
