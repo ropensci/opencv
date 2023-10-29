43d2fd46a5f25987e9690d8ae9b58f254199c39f
Jeroen Ooms <jeroenooms@gmail.com>
2023-10-29 14:28:52 +0100

    Update tag for legacy build

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -7,7 +7,7 @@ if(!file.exists("../windows/opencv/include/opencv4/opencv2/opencv.hpp")){
   } else if(getRversion() >= "4.3") {
     "https://github.com/r-windows/bundles/releases/download/opencv-4.8.1/opencv-4.8.1-ucrt-x86_64.tar.xz"
   } else {
-    "https://github.com/rwinlib/opencv/archive/refs/tags/4.8.1-1.tar.gz"
+    "https://github.com/rwinlib/opencv/archive/refs/tags/v4.8.1.tar.gz"
   }
   download.file(url, basename(url), quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
