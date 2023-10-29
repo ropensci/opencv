491b5a7503c65119f2e942c422eeb76bb0bc4860
Jeroen Ooms <jeroenooms@gmail.com>
2023-10-29 14:34:09 +0100

    Update tests

--- tests/testthat/test-qr_code.R
+++ tests/testthat/test-qr_code.R
@@ -1,7 +1,9 @@
 test_that("qr scanner works", {
-  skip_if(ocv_version() < "4.5.2")
+  skip_if(identical(Sys.info()[["sysname"]], 'Linux') && ocv_version() < "4.5.2")
   img <- ocv_read('qrtest.jpg')
   expect_s3_class(img, "opencv-image")
   txt <- ocv_qr_detect(img)
+  outimg <- ocv_qr_detect(img, draw = TRUE)
+  expect_s3_class(outimg, "opencv-image")
   expect_equal(txt, "https://www.r-project.org")
 })
